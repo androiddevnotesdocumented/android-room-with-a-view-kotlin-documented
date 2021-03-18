9669dc0289738a4654945143f1d7192916f38422
androiddevnotes <awesomedevnotes@gmail.com>
2021-03-19 03:28:57 +0900

    Updated

--- app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
+++ app/src/main/java/com/example/android/roomwordssample/MainActivity.kt
@@ -37,6 +37,11 @@ class MainActivity : AppCompatActivity() {
         WordViewModelFactory((application as WordsApplication).repository)
     }
 
+    /**
+     * Creating Activity
+     *
+     * @param savedInstanceState
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -48,10 +53,10 @@ class MainActivity : AppCompatActivity() {
 
         // Add an observer on the LiveData returned by getAlphabetizedWords.
         // The onChanged() method (the default method for our Lambda) fires when the observed data changes and the activity is in the foreground:
-        wordViewModel.allWords.observe(owner = this, onChanged = fun(words: List<Word>) {
+        wordViewModel.allWords.observe(owner = this) { words ->
             // Update the cached copy of the words in the adapter.
             words.let { adapter.submitList(it) }
-        })
+        }
 
         val fab = findViewById<FloatingActionButton>(R.id.fab)
         fab.setOnClickListener {
